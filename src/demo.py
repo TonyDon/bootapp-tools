6c167aedd0c1c538c22482c220a2b2dfb2cdad26
tangxiaodong <tangxiaodong@jd.com>
2019-02-16 13:45:32 +0800

    commit

--- src/demo.py
+++ src/demo.py
@@ -2,4 +2,4 @@ import time, os, sys
 print(os.path.dirname(os.path.realpath((__file__))))
 print(os.path.dirname(os.path.realpath(sys.executable)))
 print(getattr(sys, 'frozen', False))
-time.sleep(5)
+print(sys.argv[1])
