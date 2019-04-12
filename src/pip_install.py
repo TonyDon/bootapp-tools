c10e55c2ee484a285c407825271234c26da325cb
tangxiaodong1 <tangxiaodong1@tuhu.cn>
2019-04-12 11:43:42 +0800

    commit

---
+++ src/pip_install.py
@@ -0,0 +1,7 @@
+import os
+mod_name = input('请输入需要安装的模块名称:')
+if mod_name :
+    with os.popen('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ %s' % mod_name) as f1:
+        print(f1.read())
+    with os.popen('pip list') as f2:
+        print(f2.read())
