7ee4955b18f6178d1c25e59ab9b3717c8f28e870
tangxiaodong <tangxiaodong@jd.com>
2019-05-16 22:09:25 +0800

    commit

--- src/pip_install.py
+++ src/pip_install.py
@@ -1,8 +1,18 @@
-import os
+import os,subprocess
+from subprocess import Popen,call
 mod_name = input('请输入需要安装的模块名称:')
 if mod_name :
-    with os.popen('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ %s' % mod_name) as f1:
-        print(f1.read())
-    with os.popen('pip list') as f2:
-        print(f2.read())
-    input('\r\nEnd.')
+    with Popen('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ %s' % mod_name, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as f1:
+        while True:
+            buff = f1.stdout.readline()
+            ret = Popen.poll(f1)
+            line = None
+            if buff :
+                line = buff.decode(encoding='utf-8').strip('\r\n')
+                print('...',line)
+            if  ret is not None:
+                print('\r\n...exec finish.')
+                break
+            
+    call('pip list', shell=True)
+    input('\r\n...End.')
